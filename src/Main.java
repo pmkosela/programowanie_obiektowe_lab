f26f9bb2678999afdd79d3f970c6804e09e07769
Piotr Kosela <piotr.kosela@mail.umcs.pl>
2023-04-14 12:32:40 +0200

    After lab6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        FileCommanderCLI fc = new FileCommanderCLI(System.in, System.out);
-        fc.eventLoop();
+        CustomList<String>customList = new CustomList<String>();
+        CustomList<Type>customListT = new CustomList<Type>();
+        Type a = new Type();
+        Type b = new Type();
+        customList.addLast("a");
+        customList.addLast("b");
+        customListT.addLast(a);
+        customListT.addLast(b);
     }
 }
